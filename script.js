daf21cfc9c0de0bed693a5e78da37fc4051b4190
Jekoff <jlanloze@gmail.com>
2023-10-08 23:47:17 +0200

    commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById('input');
 const listContainer = document.getElementBuyId('list-container');
+
 function addTask(){
     if(inputBox.value === ''){
         alert('You must write something');
@@ -7,6 +8,20 @@ function addTask(){
         let li = document.createElementElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = '';
+
     
 }
+
+listContainer.addEventListener('click', function{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+}, false);
